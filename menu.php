55ad5bedfab7079f0c1e09646f84a9ff71cc5806
eudy1990p <eudy1990@gmail.com>
2016-12-28 14:57:15 -0400

    mejoras para fondos de login y registro

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://eudy.260mb.net">Agimerca</a>
+          <a class="navbar-brand" href="index.php">Agimerca</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
