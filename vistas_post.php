afce26ebed577c84ea2291ed14359c29a6eef900
eudy1990p <eudy1990@gmail.com>
2016-12-27 20:23:12 -0400

    mejoras para los comentarios

--- vistas_post.php
+++ vistas_post.php
@@ -30,6 +30,22 @@ while($resComent = mysqli_fetch_array($resultComent)){
 		
 	}
 ?>
+<ul class="media-list">
+  <li class="media">
+    <div class="media-left">
+      <a href="#">
+        <img class="media-object" src="..." alt="...">
+      </a>
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading">Media heading</h4>
+      ...
+    </div>
+  </li>
+</ul>
+
+
+
 <div style="display:none;" id="comentario<?php echo $contador; ?>" class="row">
 	<form method="post" action="" >
 		<input type="hidden"  id="mostrar<?php echo $contador; ?>" value="0"/>
