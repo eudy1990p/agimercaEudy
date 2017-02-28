09fd7c03c002db9443232bc72b65a95f80a22cde
tracamandaca <esmarlin9@gmail.com>
2017-02-28 11:54:16 -0400

    modal de imagenes

--- ver_galeria.php
+++ ver_galeria.php
@@ -11,8 +11,31 @@
 
 	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
     <script>tinymce.init({ selector:'textarea' });</script>
-	<script type='text/javascript' src="js/jspdf.debug.js"></script>  	
+	<!-- <script type='text/javascript' src="js/jspdf.debug.js"></script>  	 -->
 	  
+
+    <div id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Galeria</h4>
+                    <span class="glyphicon glyphicon-right"></span>
+                    
+                    <button class="pull-left  btn btn-default" id="btn-atras"><span class="glyphicon glyphicon-chevron-left"></span></button>
+                    <button class="pull-right btn btn-default" id="btn-adelante"><span class="glyphicon glyphicon-chevron-right"></span></button>
+                </div>
+                <div class="modal-body">
+                	<img id="imagen-modal" style="width: 90%" class="img-responsive" src=""/>
+                </div>
+                <div class="modal-footer">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 	<div class="page-header">
 		        <h1><span class="glyphicon glyphicon-file" aria-hidden="true"></span> 
 		        	Galeria
@@ -52,15 +75,16 @@
 								$resultado = mysqli_query($c->getContect(),$sql) or die(mysqli_error($c->getContect()));
 
 								while($datos = mysqli_fetch_array($resultado)){
+									static $contador = 0;
 									echo 
 									"
 									  <div class='col-xs-4'>
-									    <a href='#' class='thumbnail'>
-									      <img src='imagenes_galeria/$datos[url_img]'>
+									    <a href='#' class='thumbnail '>
+									      <img class='imagen' src='imagenes_galeria/$datos[url_img]' title='$contador'>
 									    </a>
 									  </div>
-									"
-									;
+									";
+									$contador++;
 								}
 							}
 						?>
@@ -111,7 +135,56 @@
 	
 </div>
 
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+		
+		imagen_actual=0;
 
+	    $(document).on("click",".imagen",function(){
+
+
+	    	var imagen=$(this);
+	    	var url = imagen.attr('src');
+
+	    	imagenes = [];
+	    	imagen_actual=parseInt(imagen.attr('title'));
+	    	console.log("indise "+imagen.attr('title'));
+
+	    	$('.imagen').each(function(e){
+
+	    		imagenes.push($(this).attr('src'));
+	    		
+	    	});
+
+	    	// console.log(imagenes);
+	    	// console.log(url);
+	    	$("#imagen-modal").attr('src',url);
+	       
+
+	        $("#myModal").modal('show');
+
+	    });
+
+	    $("#btn-adelante").click(function(){
+	    		imagen_actual++;
+	    		if(imagen_actual>=imagenes.length){
+	    			imagen_actual=0;
+	    		}
+				$('#imagen-modal').attr('src',imagenes[imagen_actual]);	
+	    });
+	    $("#btn-atras").click(function(){
+    		imagen_actual--;
+    		if(imagen_actual<0){
+    			imagen_actual=imagenes.length-1;
+    		}
+			$('#imagen-modal').attr('src',imagenes[imagen_actual]);
+
+	    });
+
+	});
+
+</script>
 
 <?php
 	require_once("footer.php");
--- ver_galeria_perfil_usuario.php
+++ ver_galeria_perfil_usuario.php
@@ -1,3 +1,23 @@
+    <div id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Galeria</h4>
+                    <span class="glyphicon glyphicon-right"></span>
+                    
+                    <button class="pull-left  btn btn-default" id="btn-atras"><span class="glyphicon glyphicon-chevron-left"></span></button>
+                    <button class="pull-right btn btn-default" id="btn-adelante"><span class="glyphicon glyphicon-chevron-right"></span></button>
+                </div>
+                <div class="modal-body">
+                	<img id="imagen-modal" style="width: 90%" class="img-responsive" src=""/>
+                </div>
+                <div class="modal-footer">
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 <div class="row">
 		<div class="col-xs-12">
@@ -35,15 +55,16 @@
 								$resultado = mysqli_query($c->getContect(),$sql) or die(mysqli_error($c->getContect()));
 
 								while($datos = mysqli_fetch_array($resultado)){
+									static $contador=0;
 									echo 
 									"
 									  <div class='col-xs-4'>
-									    <a href='#' class='thumbnail'>
-									      <img src='imagenes_galeria/$datos[url_img]'>
+									    <a href='#' class='thumbnail '>
+									      <img class='imagen' src='imagenes_galeria/$datos[url_img]' title='$contador'>
 									    </a>
 									  </div>
-									"
-									;
+									";
+									$contador++;
 								}
 							}
 						?>
@@ -56,3 +77,58 @@
 	</div>
 	
 </div>
+
+
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+		
+		imagen_actual=0;
+
+	    $(document).on("click",".imagen",function(){
+
+
+	    	var imagen=$(this);
+	    	var url = imagen.attr('src');
+
+	    	imagenes = [];
+	    	imagen_actual=parseInt(imagen.attr('title'));
+	    	console.log("indise "+imagen.attr('title'));
+
+	    	$('.imagen').each(function(e){
+
+	    		imagenes.push($(this).attr('src'));
+	    		
+	    	});
+
+	    	console.log(imagenes);
+	    	console.log(url);
+	    	$("#imagen-modal").attr('src',url);
+	       
+
+	        $("#myModal").modal('show');
+
+	    });
+
+	    $("#btn-adelante").click(function(){
+	    		imagen_actual++;
+	    		if(imagen_actual>=imagenes.length){
+	    			imagen_actual=0;
+	    		}
+				$('#imagen-modal').attr('src',imagenes[imagen_actual]);				
+	    	
+	    	console.log(imagen_actual)
+	    });
+	    $("#btn-atras").click(function(){
+    		imagen_actual--;
+    		if(imagen_actual<0){
+    			imagen_actual=imagenes.length-1;
+    		}
+			$('#imagen-modal').attr('src',imagenes[imagen_actual]);
+
+			console.log(imagen_actual)
+	    });
+
+	});
+
+</script>
